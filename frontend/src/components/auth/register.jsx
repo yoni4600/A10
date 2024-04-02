cc3bf667bba4c5bcdd720a2b03ca93402a2b61e3
yoni4600 <yoni4600@gmail.com>
2024-04-02 21:51:10 +0300

    fix

--- frontend/src/components/auth/register.jsx
+++ frontend/src/components/auth/register.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { fetchRegister } from '../../services/authService';
-import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const [username, setUsername] = useState('');
