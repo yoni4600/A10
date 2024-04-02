6428441d2fd9c19281ad8730534114a5707eb7c5
ChayFadida <chayfadida1997@gmail.com>
2024-04-02 18:49:57 +0300

    change visual

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { loginUser, registerUser, validateToken , updateUserAvailability } from '../controllers/userController.js';
+import { loginUser, registerUser, validateToken , updateUserAvailability, logout } from '../controllers/userController.js';
 
 const router = express.Router();
 
@@ -9,6 +9,6 @@ export function userRoutes(client) {
     router.post('/register', (req, res) => registerUser(req, res, client));
     router.post('/validateToken', (req, res) => validateToken(req, res, client));
     router.post('/updateAvailability', (req, res) => updateUserAvailability(req, res, client));
-
+    router.post('/logout', (req, res) => logout(req, res, client));
     return router;
 }
