25df505d5bc8f6819eb8f7e772482bfa54835f82
liorzucker <93440030+liorzucker@users.noreply.github.com>
2024-04-03 20:48:01 +0300

    fix quiz controller

--- backend/controllers/quizzesController.js
+++ backend/controllers/quizzesController.js
@@ -14,23 +14,24 @@ export async function getAllQuiezzes(req, res, client) {
 
 export async function getQuizById(req, res, client) {
     try {
+
         const quizId = req.params.id; // Assuming the ID is provided as a request parameter
         const database = client.db('pilokdb');
         const queizzes = database.collection('Quizzes');
 
         // Convert the string ID to ObjectId
-        const objectId = new (quizId);
+        const objectId = new ObjectId (quizId);
 
         // Find the lesson by ID
         const quiz = await queizzes.findOne({ _id: objectId });
 
         if (!quiz) {
-            res.status(404).send("Lesson not found");
+            res.status(404).send("Quiz not found");
         } else {
             res.json(quiz);
         }
     } catch (e) {
-        res.status(500).send("Error fetching lesson by ID");
+        res.status(500).send("Error fetching quiz by ID");
         console.error(e);
     }
 }
