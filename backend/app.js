09999ebb8f387a88cc0d43be8b24b7aa0789cb9b
ChayFadida <chayfadida1997@gmail.com>
2024-04-19 18:03:04 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -16,8 +16,7 @@ export function createApp(client) {
 
     // Use application-level middleware for common functionality
     app.use(cors({
-        origin: ['https://a10-frontend-lac.vercel.app:443'],
-        optionsSuccessStatus: 200,
+        origin: ['https://a10-frontend-lac.vercel.app'],
         credentials: true // Allow credentials (cookies)
     }));
 
