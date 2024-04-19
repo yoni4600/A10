f301df8e6643bf99c02b6b476a50ba64436e0440
Chay Fadida <122904042+ChayFadida@users.noreply.github.com>
2024-04-19 16:57:04 +0300

    Update config.js

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -3,8 +3,8 @@ const DEFAULT_HOST = 'a10-backend.vercel.app';
 const DEFAULT_PORT = 443;
 
 // Get the host and port from environment variables, if available
-const HOST = process.env.HOST || DEFAULT_HOST;
-const PORT = process.env.PORT || DEFAULT_PORT;
+const HOST = DEFAULT_HOST;
+const PORT = DEFAULT_PORT;
 
 module.exports = {
   HOST,
